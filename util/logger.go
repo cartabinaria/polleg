a82dc51eb08b7e2d7e1f68c9fa691cdb9305bdb9
Musiani Samuele <samuele.musiani.03@gmail.com>
2025-09-01 16:54:42 +0200

    fix: ip on logger middleware

--- util/logger.go
+++ util/logger.go
@@ -8,7 +8,11 @@ import (
 
 func NewLoggerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(time.Now().Format("2006/01/02 15:04:05"), "HTTP", r.Method, r.URL.Path, r.RemoteAddr)
+		ip := r.Header.Get("X-Forwarded-For")
+		if ip == "" {
+			ip = r.RemoteAddr
+		}
+		fmt.Println(time.Now().Format("2006/01/02 15:04:05"), "HTTP", r.Method, r.URL.Path, ip)
 		next.ServeHTTP(w, r)
 	})
 }
