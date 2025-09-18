15d234899959e73198dea899b62a653c0d9f9a6d
Samuele Musiani <samu@teapot.ovh>
2025-09-18 10:30:05 +0200

    Revert "add: log request response code"
    This reverts commit d9722661cb49630dea78313090c7c01b154962ac.

--- util/logger.go
+++ util/logger.go
@@ -6,32 +6,13 @@ import (
 	"time"
 )
 
-type statusRecorder struct {
-	http.ResponseWriter
-	status int
-}
-
-func (r *statusRecorder) WriteHeader(code int) {
-	r.status = code
-	r.ResponseWriter.WriteHeader(code)
-}
-
 func NewLoggerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ip := r.Header.Get("X-Forwarded-For")
 		if ip == "" {
 			ip = r.RemoteAddr
 		}
-
-		rec := &statusRecorder{
-			ResponseWriter: w,
-			status:         200, // default status to 200
-		}
-
-		next.ServeHTTP(rec, r)
-		fmt.Println(
-			time.Now().Format("2006/01/02 15:04:05"),
-			"HTTP", r.Method, rec.status, r.URL.Path, ip,
-		)
+		fmt.Println(time.Now().Format("2006/01/02 15:04:05"), "HTTP", r.Method, r.URL.Path, ip)
+		next.ServeHTTP(w, r)
 	})
 }
